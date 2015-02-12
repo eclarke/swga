04dfc2606c667041be4bccc57c0abdbdd2ccaa1e
Erik Clarke <ecl@mail.med.upenn.edu>
2015-02-11 19:15:28 -0500

    final commit before we move to master

---
+++ tests/conftest.py
@@ -0,0 +1,28 @@
+import pytest
+
+import swga.database
+from swga.database import Primer, Set, PrimerSet
+
+
+@pytest.fixture
+def seqs():
+    return ['A' * (i + 1) for i in range(20)]
+
+@pytest.fixture(scope="function")
+def initdb(request):
+    swga.database.db.init(":memory:")
+    swga.database.db.create_tables([Primer, Set, PrimerSet])
+    def fin():
+        print ("Closing database")
+        swga.database.db.close()
+    request.addfinalizer(fin)
+
+@pytest.fixture
+def tprimers(seqs):
+    return [Primer.create(seq=seq) for seq in seqs]
+
+@pytest.fixture
+def tset():
+    return Set.create(_id=1, score=100) 
+
+
