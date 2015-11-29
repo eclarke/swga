b5306ebd6ec1a9e2d350283626a223b6703945d6
Erik Clarke <erikclarke@gmail.com>
2015-11-28 23:33:36 -0500

    tests moved into package

--- tests_new/context.py
+++
@@ -1,9 +0,0 @@
-"""
-Allows the tests to import the source code within this directory without
-running `setup.py develop` or `pip install --editable`.
-From Kenneth Reitz (http://www.kennethreitz.org/repository-structure-and-python/)
-"""
-import os
-import sys
-sys.path.insert(0, os.path.abspath('..'))
-import swga
